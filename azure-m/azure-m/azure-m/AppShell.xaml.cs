6989c314a82192cc2d696539878e705e4d5270d6
www159 <2107795244@qq.com>
2022-06-21 13:36:18 +0800

    feat:增加移动端

---
+++ azure-m/azure-m/azure-m/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using azure_m.ViewModels;
+using azure_m.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace azure_m
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ azure-m/azure-m/azure-m/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using azure_m.Models;
+using azure_m.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace azure_m.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
