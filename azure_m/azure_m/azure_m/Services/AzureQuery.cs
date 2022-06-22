14fb09421c316d497070398d3d1c56965a6484e6
www159 <2107795244@qq.com>
2022-06-22 16:15:00 +0800

    333

--- azure_m/azure_m/azure_m/Services/AzureQuery.cs
+++ azure_m/azure_m/azure_m/Services/AzureQuery.cs
@@ -2,51 +2,62 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Net.Http;
+using Flurl;
+using Flurl.Http;
+using System.Threading.Tasks;
 
 namespace azure_m.Services
 {
 
- 
+
     public static class QueryInfo
     {
         public const string queryPrefix = "https://management.azure.com/subscriptions/";
         public static string subscriptionId = "";
         public const string apiVersion = "2021-04-01";
-        public const string apiVersionAd = "api-version=" + apiVersion;
-        public static Dictionary<string, Object> queryList = new Dictionary<string, Object>()
-        {
-            ["resources"] = new ResourcesQuery(),
-        };
-        //public static HttpResponseMessage query(string url, HttpMethod method)
-        //{
-        //    Htt
-        //}
+        public static string apiPrefixAd { get { return queryPrefix + subscriptionId; } }
+        public static string apiVersionAd { get { return "api-version=" + apiVersion; } }
+        public static QueryList queryList = new QueryList();
     }
 
    
-  
+    public class QueryList
+    {
+        public ResourcesQuery resources = new ResourcesQuery();
+    }
 
 
     public class ResourcesQuery
-    { 
-        public string listResources(string filter = "", Nullable<int> top = null)
+    {
+        Url baseUrl = new Url(QueryInfo.apiPrefixAd).AppendPathSegment("resourcegroups"); 
+        public async Task<T> listResources<T>(string filter = "", Nullable<int> top = null)
         {
             
             if(filter is "" && top is null)
             {
-                return $"/resourcegroups?{QueryInfo.apiVersionAd}";
+                return await baseUrl.Clone().GetJsonAsync<T>();
             }
             else if(filter is "")
             {
-                return $"/resourcegroups?$top={top}&{QueryInfo.apiVersionAd}";
+                return await baseUrl.Clone().SetQueryParams(new
+                {
+                    top = top,
+                }).GetJsonAsync<T>();
             }
             else if(top is null)
             {
-                return $"/resourcegroups?$filter={filter}&{QueryInfo.apiVersionAd}";
+                return await baseUrl.Clone().SetQueryParams(new
+                {
+                    filter = filter
+                }).GetJsonAsync<T>();
             }
             else
             {
-                return $"/resourcegroups?$filter={filter}&$top={top}&{QueryInfo.apiVersionAd}";
+                return await baseUrl.Clone().SetQueryParams(new
+                {
+                    filter = filter,
+                    top = top
+                }).GetJsonAsync<T>();
             }
         }
     }
--- azure_m/azure_m/azure_m/Services/ResousesDataStore.cs
+++ azure_m/azure_m/azure_m/Services/ResousesDataStore.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using azure_m.Models;
+using azure_m.Services;
 
 namespace azure_m.Services
 {
@@ -9,11 +10,12 @@ namespace azure_m.Services
     /// 所有资源的动作
     /// </summary>
     /// <typeparam name="T">泛型是虚拟资源类，规定所有资源都有相对应的类型</typeparam>
-    //class ResourcesDataStore : IDataStore<Resources>
-    //{
-    //    public async Task<Resources> GetItemAsync(string subsScriptionId, string apiVersion)
-    //    {
-    //        return await
-    //    }
-    //}
+    class resourcesdatastore
+    {
+        public async Task<Resources[]> GetItemsAsync(string filter = "", Nullable<int> top = null)
+        {
+            return await QueryInfo.queryList.resources.listResources<Resources[]>(filter, top);
+        }
+
+    }
 }
