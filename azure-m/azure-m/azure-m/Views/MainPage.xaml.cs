8d30d915a2e37c63c0c863a7a56b2fb0f8a10bb0
Unprincess17 <202030211121@whu.edu.cn>
2022-06-22 17:04:38 +0800

    homepage 22nd june

--- azure-m/azure-m/azure-m/Views/MainPage.xaml.cs
+++ azure-m/azure-m/azure-m/Views/MainPage.xaml.cs
@@ -11,5 +11,30 @@ namespace azure_m.Views
         {
             InitializeComponent();
         }
+
+        private void swapBtnColors()
+        {
+            object bgc = RecentBtn.BackgroundColor, tc = RecentBtn.TextColor, bc = RecentBtn.BorderColor;
+            RecentBtn.BackgroundColor = FavoriteBtn.BackgroundColor;
+            RecentBtn.TextColor = FavoriteBtn.TextColor;
+            RecentBtn.BorderColor = FavoriteBtn.BorderColor;
+            FavoriteBtn.BackgroundColor = (Color)bgc; 
+            FavoriteBtn.BackgroundColor = (Color)tc;
+            FavoriteBtn.BackgroundColor = (Color)bc;
+
+        }
+
+        public void onFavoriteBtn_Clicked(object sender, EventArgs e)
+        {
+            swapBtnColors();
+            //异步 querying datas, 在此期间出现一个刷新标志
+
+        }
+
+        public void onRecentBtn_Clicked(object sender, EventArgs e)
+        {
+            swapBtnColors();
+            //异步 querying datas, 在此期间出现一个刷新标志
+        }
     }
 }
