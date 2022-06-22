900d38fec10d8b748a5b64c75117c903ff34df23
www159 <2107795244@qq.com>
2022-06-22 14:02:00 +0800

    refactor:修改合适的名字

---
+++ azure-m/azure-m/azure-m/Services/AzureQuery.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Http;
+
+namespace azure_m.Services
+{
+
+ 
+    public static class QueryInfo
+    {
+        public const string queryPrefix = "https://management.azure.com/subscriptions/";
+        public static string subscriptionId = "";
+        public const string apiVersion = "2021-04-01";
+        public const string apiVersionAd = "api-version=" + apiVersion;
+        public static Dictionary<string, Object> queryList = new Dictionary<string, Object>()
+        {
+            ["resources"] = new ResourcesQuery(),
+        };
+        //public static HttpResponseMessage query(string url, HttpMethod method)
+        //{
+        //    Htt
+        //}
+    }
+
+   
+  
+
+
+    public class ResourcesQuery
+    { 
+        public string listResources(string filter = "", Nullable<int> top = null)
+        {
+            
+            if(filter is "" && top is null)
+            {
+                return $"/resourcegroups?{QueryInfo.apiVersionAd}";
+            }
+            else if(filter is "")
+            {
+                return $"/resourcegroups?$top={top}&{QueryInfo.apiVersionAd}";
+            }
+            else if(top is null)
+            {
+                return $"/resourcegroups?$filter={filter}&{QueryInfo.apiVersionAd}";
+            }
+            else
+            {
+                return $"/resourcegroups?$filter={filter}&$top={top}&{QueryInfo.apiVersionAd}";
+            }
+        }
+    }
+}
---
+++ azure-m/azure-m/azure-m/Services/ResousesDataStore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using azure_m.Models;
+
+namespace azure_m.Services
+{
+    /// <summary>
+    /// 所有资源的动作
+    /// </summary>
+    /// <typeparam name="T">泛型是虚拟资源类，规定所有资源都有相对应的类型</typeparam>
+    class ResourcesDataStore : IDataStore<Resources>
+    {
+        public async Task<Resources> GetItemAsync(string subsScriptionId, string apiVersion)
+        {
+            return await
+        }
+    }
+}
