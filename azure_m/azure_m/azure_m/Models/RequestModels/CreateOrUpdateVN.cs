95d5a844c39fc5279b49f437e0a68ae2cead5b34
tom_glb <84129849+Di-viner@users.noreply.github.com>
2022-07-03 16:50:40 +0800

    虚拟网络创建CreateOrUpdateRequest类

---
+++ azure_m/azure_m/azure_m/Models/RequestModels/CreateOrUpdateVN.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace azure_m.Models.RequestModels
+{
+    namespace VNReuestModels
+    {
+        namespace CreateOrUpdate
+        {
+            public class CreateOrUpdateVNBody
+            {
+                public CreateOrUpdateVNProperties properties;
+
+                public string location;
+            }
+
+            public class CreateOrUpdateVNProperties
+            {
+                public AddressSpace addressSpace;
+
+                public Subnet[] subnets;
+
+                public VirtualNetworkBgpCommunities bgpCommunities;
+
+                public int flowTimeOutInMinutes;
+
+                public VirtualNetworkEncryption encryption;
+            }
+
+            public class AddressSpace
+            {
+                public string[] addressPrefixes;
+            }
+
+            public class Subnet
+            {
+                //public string etag;
+
+                //public string id;
+                
+                public string name;
+
+                public SubnetProperties properties;
+
+            }
+
+            public class SubnetProperties
+            {
+                public string addressPrefix;
+
+                public string[] addressPrefixes;
+
+                public Delegation[] delegations;
+
+                public ServiceEndpointPropertiesFormat[] serviceEndpoints;
+
+                public ServiceEndpointPolicy[] serviceEndpointPolicies;
+            }
+
+            public class Delegation
+            {
+                public string etag;
+
+                public string id;
+
+                public string name;
+
+                public DelegationProperties properties;
+
+                public string type;
+            }
+
+            public class DelegationProperties
+            {
+                public string[] actions;
+
+                public string serviceName;
+
+                //public ProvisioningState provisioningState;
+            }
+
+            public class ServiceEndpointPropertiesFormat
+            {
+                public string[] locations;
+
+                public string service;
+
+                //public ProvisioningState provisioning;
+            }
+
+            public class ServiceEndpointPolicy
+            {
+                public string id;
+
+                public string etag;
+
+                public string kind;
+
+                public string location;
+
+                public string name;
+
+                public object tags;
+
+                public string type;
+
+                //public ServiceEndpointPolicyProperties ServiceEndpointPolicy;
+            }
+
+            public class VirtualNetworkBgpCommunities
+            {
+                public string regionalCommunity;
+
+                public string virtualNetworkCommunity;
+            }
+
+            public class VirtualNetworkEncryption
+            {
+                public bool enabled;
+
+                public string enforcement;
+            }
+
+        }
+    }
+}
