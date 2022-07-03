5857ed8fdcde6352a9484386d3dc26eecc3c6f79
Unprincess17 <202030211121@whu.edu.cn>
2022-07-04 00:21:04 +0800

    feat: basic by binding and carousel page

--- azure_m/azure_m/azure_m/ViewModels/VMDetailsViewModel.cs
+++ azure_m/azure_m/azure_m/ViewModels/VMDetailsViewModel.cs
@@ -19,15 +19,48 @@ namespace azure_m.ViewModels
         public List<string> subscribesNames { get; set; }
         public string subscribeID;
         public List<string> resourceGroups { get; set; }
+        public string ResourceGroup;
+
+        public List<string> AreaSources { get; set; }
+
+        public List<string> Zones { get; set; }
+        public Command ZonesIndexChange;
+
+        public List<ImageReference> Images { get; set; }
+
+        public List<string> vmSizes { get; set; }
+
+        public List<string> ports { get; set; }
+        public Command portsChange;
+
         public CreateOrUpdateVMRequest vm = new CreateOrUpdateVMRequest();
         public VMDetailsViewModel()
         {
 #if DEBUG
             subscribes = new Dictionary<string, string> { { "免费试用", "123" } };
-            subscribesNames = subscribes.Keys.ToList();
-            resourceGroups = new List<string>() { "wfpres" };
-            Views.AddVmDetailsPage.SubscribeIndexChange += ChangeSubID;
+            resourceGroups = new List<string>{"wfpres", "wfpppres" };
+            AreaSources = new List<string> { "(Asia Pacific)Australia East", "(Asia Pacific)Southest Asia(有资格免费试用服务)", };
+            Zones = new List<string> { "Zones 1", "Zones 2", "Zones 3" };
+            Images = new List<ImageReference> { new ImageReference() { sku = "20_04-lts-gen2", publisher = "Canonical", verison = "latest", offer = "0001-com-ubuntu-server-focal" } };
+            vmSizes = new List<string> { "Standard_D2s_v3" };
+            ports = new List<string> { "HTTPS(443)", "HTTP(80)","SSH(22)"};
+
 #endif
+            subscribesNames = subscribes.Keys.ToList();
+            #region Bindings
+            Views.AddVmDetailsPage.SubscribeIndexChange += ChangeSubID;
+            Views.AddVmDetailsPage.ResourceGroupIndexChanged += (sender, e) => { vm.uri.resourceGroupName = (sender as Picker).SelectedItem.ToString(); };
+            Views.AddVmDetailsPage.AreaChanged += (sender, e) => {vm.body.location = AreaSources[(sender as Picker).SelectedIndex]; };
+            ZonesIndexChange = new Command((sender) => { 
+                //1. clear 2.load WTF why can't clear an array, can it dispose the dangling array?
+                //vm.body.zones.
+            });
+            Views.AddVmDetailsPage.ImagesIndexChanged += (sender, e) => { vm.body.properties.storageProfile.imageReference = Images[(sender as Picker).SelectedIndex]; };
+            Views.AddVmDetailsPage.vmSizeIndexChanged+=(sender, e) => { vm.body.properties.hardWareProfile.vmSize = new string[] { vmSizes[(sender as Picker).SelectedIndex] }; };
+            Views.AddVmDetailsPage.passwdComplete += (sender, e) => vm.body.properties.osProfile.adminPassword = (sender as Entry).Text.ToString();
+            portsChange = new Command((sender) => { });
+
+            #endregion
         }
 
         private void ChangeSubID(object sender, int i)
--- azure_m/azure_m/azure_m/Views/AddVMDetailsPage.xaml.cs
+++ azure_m/azure_m/azure_m/Views/AddVMDetailsPage.xaml.cs
@@ -22,30 +22,68 @@ namespace azure_m.Views
 
         }
 
-        private void UserName_TextChanged(object sender, TextChangedEventArgs e)
+        private void UserName_Completed(object sender, EventArgs e)
         {
 
         }
 
-        private void passwd_TextChanged(object sender, TextChangedEventArgs e)
+        private void passwd_Completed(object sender, EventArgs e)
         {
 
         }
 
-        private void passwd2_TextChanged(object sender, TextChangedEventArgs e)
+        public static EventHandler passwdComplete;
+        private void passwd2_Completed(object sender, EventArgs e)
         {
-
+            //if(valid)
+            passwdComplete?.Invoke(sender, EventArgs.Empty);
         }
 
+        public static event EventHandler ResourceGroupIndexChanged;
         private void resourcesGroup_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            ResourceGroupIndexChanged?.Invoke(sender, EventArgs.Empty);
         }
 
         public static event EventHandler<int> SubscribeIndexChange;
         private void subscribe_SelectedIndexChanged(object sender, EventArgs e)
         {
             SubscribeIndexChange?.Invoke(sender, subscribe.SelectedIndex);
+        }
+
+        public static event EventHandler AreaChanged;
+        private void areas_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (areas.SelectedIndex != -1) { AreaChanged?.Invoke(sender, EventArgs.Empty); }
+        }
+
+        public static event EventHandler accessIndexChanged;
+        private void availabilty_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (availabilty.SelectedIndex != -1) { accessIndexChanged?.Invoke(sender, EventArgs.Empty); }
+        }
+
+        
+        private void safetymode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public static event EventHandler ImagesIndexChanged;
+        private void images_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (images.SelectedIndex != -1) { ImagesIndexChanged?.Invoke (sender, EventArgs.Empty); }
+        }
+
+        public static event EventHandler vmSizeIndexChanged;
+        private void vmSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(vmSize.SelectedIndex!=-1) { vmSizeIndexChanged?.Invoke(sender, EventArgs.Empty); }
+        }
+
+        private void openentryPort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            port.IsVisible = openentryPort.SelectedIndex==0?false:true;
         }
     }
 }
