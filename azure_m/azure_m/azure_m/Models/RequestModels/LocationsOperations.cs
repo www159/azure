690d7e7ae724c5968f1221c3d6ed3f617255ad0a
aisord <1959028047@qq.com>
2022-07-06 20:56:14 +0800

    task

---
+++ azure_m/azure_m/azure_m/Models/RequestModels/LocationsOperations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace azure_m.Models.RequestModels
+{
+    namespace Locations
+    {
+            public class LocationsBody
+            {
+
+            }
+
+            public class LocationsUri
+            {
+
+            }
+
+            public class LocationsRequest:IRequest<LocationsUri,LocationsBody>
+            {
+
+            }
+    }
+}
---
+++ azure_m/azure_m/azure_m/Models/ResponseModels/LocationResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace azure_m.Models.ResponseModels
+{
+    namespace ResponseModels
+    {
+        public class LocationResponse
+        {
+            public string name { get; set; }
+            public string displayName { get; set; }
+        }
+    }
+}
