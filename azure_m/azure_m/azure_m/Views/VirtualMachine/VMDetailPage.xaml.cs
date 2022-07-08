b2de8c82f39518395ed98b1da1d0c09a380bd84d
Unprincess17 <2020302111121@whu.edu.cn>
2022-07-08 17:13:54 +0800

    feat: VMDetailPage

--- azure_m/azure_m/azure_m/Views/VirtualMachine/VMDetailPage.xaml.cs
+++ azure_m/azure_m/azure_m/Views/VirtualMachine/VMDetailPage.xaml.cs
@@ -9,12 +9,15 @@ using Xamarin.Forms.Xaml;
 
 namespace azure_m.Views
 {
+    using azure_m.ViewModels;
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class VMDetailPage : ContentPage
     {
         public VMDetailPage()
         {
             InitializeComponent();
+            BindingContext = new VMDetailViewModel();
+            vnlbl.Text = (BindingContext as VMDetailViewModel)?.RGName + "-vnet/default";
         }
     }
 }
